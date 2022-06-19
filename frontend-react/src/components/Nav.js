cfd54dbd3e06fb6c0d5de2079c58c37870c1f7e3
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-19 12:35:03 +0300

    style - complete nav component style

--- frontend-react/src/components/Nav.js
+++ frontend-react/src/components/Nav.js
@@ -1,38 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import logo from "../assets/logo.png";
 
 function Nav() {
   return (
-    <nav class="nav">
-      <ul class="nav__list nav__list--primary">
-        <li class="nav__item">
-          <a href="#" class="nav__link">
-            Home
-          </a>
-        </li>
-        <li class="nav__item">
-          <a id="get_categories" href="#" class="nav__link">
-            About
-          </a>
-        </li>
-        <li class="nav__item">
-          <a href="#" class="nav__link">
-            Contact
-          </a>
-        </li>
-      </ul>
-      <ul class="nav__list nav__list--secondary">
-        <li class="nav__item">
-          <a href="#" class="nav__link" id="username">
-            login
-          </a>
-        </li>
-        <li class="nav__item">
-          <a href="#" class="nav__link nav__link--button">
-            register
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <div class="container row">
+        <a class="logo row" href="#">
+          <img src={logo} alt="survey builder" />
+        </a>
+        <nav class="nav">
+          <ul class="nav__list nav__list--primary">
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Home
+              </a>
+            </li>
+            <li class="nav__item">
+              <a id="get_categories" href="#" class="nav__link">
+                About
+              </a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <ul class="nav__list nav__list--secondary">
+            <li class="nav__item">
+              <a href="#" class="nav__link" id="username">
+                Register
+              </a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="btn nav__link nav__link--button">
+                Login
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
